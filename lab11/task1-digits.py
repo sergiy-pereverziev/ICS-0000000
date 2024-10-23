a25434db9c122b18e93f42f3a8383ec85757dd28
Sergiy Pereverziev <sergiy.pereverziev@nearmap.com>
2024-10-24 01:03:35 +0300

    Lab 11

---
+++ lab11/task1-digits.py
@@ -0,0 +1,5 @@
+def main():
+    print("CSV report on the input number")
+        
+if __name__ == "__main__":
+    main()
---
+++ lab11/task2-carplates.py
@@ -0,0 +1,5 @@
+def main():
+    print("Searching on the JSON vehicle database")
+        
+if __name__ == "__main__":
+    main()
